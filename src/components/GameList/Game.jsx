4247c05af7acfd19fc3d313b072b14eaf1cb2056
liaopeichen <152478331+liaopeichen@users.noreply.github.com>
2024-02-22 16:13:01 +0800

    feat: implement Game component with CSS styling and GameList component with data retrieval

---
+++ src/components/GameList/Game.jsx
@@ -0,0 +1,11 @@
+import "./Game.css";
+
+export default function Game({ title, image, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
