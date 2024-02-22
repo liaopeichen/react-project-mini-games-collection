ceead9a1dfd33fd641c50dae4b0f0f1e6a8e708e
liaopeichen <152478331+liaopeichen@users.noreply.github.com>
2024-02-22 15:29:29 +0800

    fix: change "Pick Game" to "Pig Game"

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import headerIcon from "../../assets/stylized-atom.png";
 import "./Header.css";
 
-const games = ["Guess Number", "Pick Game", "Tic Tac Toe", "Timer Challenge"];
+const games = ["Guess Number", "Pig Game", "Tic Tac Toe", "Timer Challenge"];
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
