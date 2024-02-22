700ff4a124c6a8da5a8caf232bed1fa5cac3a245
liaopeichen <152478331+liaopeichen@users.noreply.github.com>
2024-02-22 16:10:09 +0800

    refactor: remove default Vite React project settings and images

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Header from "./components/Header/Header";
+import Header from "./components/Header/Header.jsx";
+import GameList from "./components/GameList/GameList.jsx";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <GameList />
+      </main>
     </>
   );
 }
