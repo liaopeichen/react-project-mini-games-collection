c23cece50b3c55b8d37e04924aaebe40dfb1edfb
liaopeichen <152478331+liaopeichen@users.noreply.github.com>
2024-02-23 18:58:54 +0800

    feat: implement Footer component and apply CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import GameList from "./components/GameList/GameList.jsx";
 import HowToPlay from "./components/HowToPlay/HowToPlay.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Header />
       <main>
         <GameList />
         <HowToPlay />
       </main>
-    </>
+      <Footer />
+    </div>
   );
 }
 
