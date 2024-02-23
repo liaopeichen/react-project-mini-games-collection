5c05a3241cd7a49ff86865bc22662f6bde444a25
liaopeichen <152478331+liaopeichen@users.noreply.github.com>
2024-02-23 09:47:40 +0800

    refactor: restructure HOW_TO_PLAY data; add and import image files; update GAME_LIST image sources

--- src/data.js
+++ src/data.js
@@ -1,29 +1,33 @@
-import guessnumberImg from "./assets/guess-number.png";
-import piggameImg from "./assets/pig-game.png";
-import tictactoeImg from "./assets/tic-tac-toe.png";
-import timerchallengeImg from "./assets/timer-challenge.png";
+import guessNumberIcon from "./assets/guess-number-icon.png";
+import pigGameIcon from "./assets/pig-game-icon.png";
+import ticTacToeIcon from "./assets/tic-tac-toe-icon.png";
+import timerChallengeIcon from "./assets/timer-challenge-icon.png";
+import guessNumberScreenShot from "./assets/guess-number-screenshot.png";
+import pigGameScreenShot from "./assets/pig-game-screenshot.png";
+import ticTacToeScreenShot from "./assets/tic-tac-toe-screenshot.png";
+import timerChallengeScreenShot from "./assets/timer-challenge-screenshot.png";
 
 export const GAME_LIST = [
   {
-    image: guessnumberImg,
+    image: guessNumberIcon,
     title: "Guess Number",
     description:
       "Guess the number between 1 and 20 in 20 attempts or fewer. Aim for a low score to win. Good luck!",
   },
   {
-    image: piggameImg,
+    image: pigGameIcon,
     title: "Pig Game",
     description:
       "Roll the die to accumulate points, avoiding a roll of 1 to continue your turn. Reach 100 points to win!",
   },
   {
-    image: tictactoeImg,
+    image: ticTacToeIcon,
     title: "Tic Tac Toe",
     description:
       "Strive to create a line of three symbols on a 3x3 grid. A timeless game of strategy and anticipation!",
   },
   {
-    image: timerchallengeImg,
+    image: timerChallengeIcon,
     title: "Timer Challenge",
     description:
       "Choose from various difficulty levels, beat the clock, and aim for high scores in this thrilling challenge!",
@@ -33,43 +37,41 @@ export const GAME_LIST = [
 export const HOW_TO_PLAY = {
   guessnumber: {
     title: "Guess Number",
-    description: `
-    <ol>
-        <li>Try to guess the hidden number between 1 and 20.</li>
-        <li>You have a maximum of 20 attempts.</li>
-        <li>The fewer attempts you use, the higher your final score.</li>
-        <li>You can always try again for a better score.</li>
-        <li>Good luck!</li>
-    </ol>
-  `,
+    gamerule: [
+      "Try to guess the hidden number between 1 and 20.",
+      "You have a maximum of 20 attempts.",
+      "The fewer attempts you use, the higher your final score.",
+      "You can always try again for a better score.",
+    ],
+    screenshot: guessNumberScreenShot,
   },
   piggame: {
     title: "Pig Game",
-    description: `
-    <ol> 
-         <li>Roll the die to accumulate points.</li> 
-         <li>Avoid rolling a 1, which ends your turn and forfeits points for that round.</li> 
-         <li>Choose to hold to add your round's points to your total score.</li> 
-         <li>Reach 100 points first to win the game.</li> 
-    </ol>`,
+    gamerule: [
+      "Roll the die to accumulate points.",
+      "Avoid rolling a 1, which ends your turn and forfeits points for that round.",
+      "Choose to hold to add your round's points to your total score.",
+      "Reach 100 points first to win the game.",
+    ],
+    screenshot: pigGameScreenShot,
   },
   tictactoe: {
     title: "Tic Tac Toe",
-    description: `
-    <ol>
-        <li>Players take turns placing their symbol on an empty square.</li>
-        <li>The first to get three in a row, column, or diagonal wins.</li>
-        <li>If the grid is full and no one wins, it's a draw.</li>
-    </ol>`,
+    gamerule: [
+      "Players take turns placing their symbol on an empty square.",
+      "The first to get three in a row, column, or diagonal wins.",
+      "If the grid is full and no one wins, it's a draw.",
+    ],
+    screenshot: ticTacToeScreenShot,
   },
   timerchallenge: {
     title: "Timer Challenge",
-    description: `
-    <ol>
-        <li>Select a challenge difficulty and start the timer.</li>
-        <li>Try to complete the challenge within the specified time limit.</li>
-        <li>Stop the timer to see your score when you finish or when time runs out.</li>
-        <li>Your score is based on how quickly you complete the challenge.</li>
-    </ol>`,
+    gamerule: [
+      "Select a challenge difficulty and start the timer.",
+      "Try to complete the challenge within the specified time limit.",
+      "Stop the timer to see your score when you finish or when time runs out.",
+      "Your score is based on how quickly you complete the challenge.",
+    ],
+    screenshot: timerChallengeScreenShot,
   },
 };
