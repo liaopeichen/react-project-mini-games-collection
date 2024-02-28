0b0902cc511908af2c80fb129449d44ec600e858
liaopeichen <152478331+liaopeichen@users.noreply.github.com>
2024-02-28 13:42:23 +0800

    feat: update Game button links

--- src/data.js
+++ src/data.js
@@ -1,33 +1,37 @@
-import guessNumberIcon from "./assets/guess-number-icon.png";
-import pigGameIcon from "./assets/pig-game-icon.png";
-import ticTacToeIcon from "./assets/tic-tac-toe-icon.png";
-import timerChallengeIcon from "./assets/timer-challenge-icon.png";
-import guessNumberScreenShot from "./assets/guess-number-screenshot.png";
-import pigGameScreenShot from "./assets/pig-game-screenshot.png";
-import ticTacToeScreenShot from "./assets/tic-tac-toe-screenshot.png";
-import timerChallengeScreenShot from "./assets/timer-challenge-screenshot.png";
+import guessNumberIcon from "assets/guess-number-icon.png";
+import pigGameIcon from "assets/pig-game-icon.png";
+import ticTacToeIcon from "assets/tic-tac-toe-icon.png";
+import timerChallengeIcon from "assets/timer-challenge-icon.png";
+import guessNumberScreenShot from "assets/guess-number-screenshot.png";
+import pigGameScreenShot from "assets/pig-game-screenshot.png";
+import ticTacToeScreenShot from "assets/tic-tac-toe-screenshot.png";
+import timerChallengeScreenShot from "assets/timer-challenge-screenshot.png";
 
 export const GAME_LIST = [
   {
     image: guessNumberIcon,
+    url: "https://liaopeichen.github.io/javascript-game-guess-number/",
     title: "Guess Number",
     description:
       "Guess the number between 1 and 20 in 20 attempts or fewer. Aim for a low score to win. Good luck!",
   },
   {
     image: pigGameIcon,
+    url: "https://liaopeichen.github.io/javascript-pig-game/",
     title: "Pig Game",
     description:
       "Roll the die to accumulate points, avoiding a roll of 1 to continue your turn. Reach 100 points to win!",
   },
   {
     image: ticTacToeIcon,
+    url: "https://react-tic-tac-toe-jvtd.onrender.com/",
     title: "Tic Tac Toe",
     description:
       "Strive to create a line of three symbols on a 3x3 grid. A timeless game of strategy and anticipation!",
   },
   {
     image: timerChallengeIcon,
+    url: "https://react-timer-challenges.onrender.com/",
     title: "Timer Challenge",
     description:
       "Choose from various difficulty levels, beat the clock, and aim for high scores in this thrilling challenge!",
